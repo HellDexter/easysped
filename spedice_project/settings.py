831b17a5a22846e1640249d61faf67d84f85cb5b
Pavel Bertelmann <144422764+HellDexter@users.noreply.github.com>
2025-09-30 09:52:09 +0200

    Update settings.py

--- spedice_project/settings.py
+++ spedice_project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     'logistika',
     'crispy_forms',
+    'storages', # Přidáno pro django-storages
 ]
 
 MIDDLEWARE = [
@@ -151,9 +152,24 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Konfigurace WhiteNoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Nastavení pro AWS S3 (použije se pouze v produkci na Renderu)
+if 'RENDER' in os.environ:
+    # Nastavení pro ukládání médií na S3
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_FILE_OVERWRITE = False
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
+
+if 'RENDER' in os.environ:
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+else:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
